f695386a7b280e6bf665952d203c921c1fd33e10
imfsdev <mingsong.webdev@gmail.com>
2020-11-22 04:09:16 -0500

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,12 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
+import ReactCodeInput from "react-verification-code-input";
 import App from "./App";
 
-test("Renders Verification Code Input Component", () => {
+test("Renders Verification Component", () => {
   render(<App />);
+});
+
+test("Renders Verification Code Input Component", () => {
+  render(<ReactCodeInput />);
 });
